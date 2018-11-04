acf013607bb35dc3b389f6b81bb2f1b3008c797c
PETER MOORE <506953@hull.ac.uk>
2018-11-04 14:50:04 +0000

    Added timer

---
+++ Assets/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    float timeRemaining = 120.0f;
+    public Text timeText;
+	// Use this for initialization
+	void Start ()
+    {
+        //text = GetComponent<GUIText>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeRemaining -= Time.deltaTime;
+
+        float minutes = Mathf.Floor(timeRemaining / 60);
+        float seconds = timeRemaining % 60;
+        timeText.text = "Time " + minutes + ":" +  Mathf.RoundToInt(seconds);
+        if (timeRemaining <= 0)
+            SceneManager.LoadScene(0);
+        
+	}
+}
