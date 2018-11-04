cf817a3871fd773bc54a2a13a6f97fb35b1b74a8
Arjun Chauhan <arjunchauhan007@gmail.com>
2018-11-04 10:52:55 +0000

    Added sprites

---
+++ Assets/Scripts/CollectionPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public int GetValue(string tag)
+    {
+        if (tag == "Small Debris")
+            return 20;
+        else if (tag == "Medium Debris")
+            return 50;
+        else if (tag == "Large Debris")
+            return 100;
+        else
+            return 0;
+    }
+}
