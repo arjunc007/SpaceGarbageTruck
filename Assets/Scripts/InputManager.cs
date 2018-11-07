6533d08686001392478155be306b901ed1fa515d
arjunc007 <arjunchauhan007@gmail.com>
2018-11-07 01:14:40 +0000

    Modified Build Settings

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+	}
+}
